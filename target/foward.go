4f8c216a40d3be4b735ad984c805d456b516c5d5
ICKelin <yingjiu.hulu@gmail.com>
2021-11-30 20:12:23 +0800

    fix: fix target connection closed

--- target/foward.go
+++ target/foward.go
@@ -44,15 +44,6 @@ func (f *Forward) ListenAndServe() error {
 func (f *Forward) forward(conn transport.Conn) {
 	defer conn.Close()
 
-	// dial target address
-	targetConn, err := net.Dial("tcp", f.targetAddr)
-	if err != nil {
-		logs.Error("dial target fail: %v", err)
-		return
-	}
-	logs.Debug("open a new connection to target %v", f.targetAddr)
-	defer targetConn.Close()
-
 	for {
 		stream, err := conn.AcceptStream()
 		if err != nil {
@@ -60,33 +51,43 @@ func (f *Forward) forward(conn transport.Conn) {
 			break
 		}
 
-		go f.handleStream(stream, targetConn)
+		go f.handleStream(stream)
 	}
 }
 
-func (f *Forward) handleStream(stream transport.Stream, conn net.Conn) {
+func (f *Forward) handleStream(stream transport.Stream) {
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 
+	// dial target address
+	targetConn, err := net.Dial("tcp", f.targetAddr)
+	if err != nil {
+		logs.Error("dial target fail: %v", err)
+		return
+	}
+	logs.Debug("open a new connection to target %v", f.targetAddr)
+
 	go func() {
 		defer wg.Done()
+		defer targetConn.Close()
 		defer stream.Close()
 		obj := f.mempool.Get()
 		defer f.mempool.Put(obj)
 		buf := obj.([]byte)
-		io.CopyBuffer(conn, stream, buf)
+		io.CopyBuffer(targetConn, stream, buf)
 	}()
 
 	go func() {
 		defer wg.Done()
+		defer targetConn.Close()
 		defer stream.Close()
 		obj := f.mempool.Get()
 		defer f.mempool.Put(obj)
 		buf := obj.([]byte)
-		_, err := io.CopyBuffer(stream, conn, buf)
+		_, err := io.CopyBuffer(stream, targetConn, buf)
 		logs.Debug("close copy conn->stream: %v", err)
 	}()
 
 	wg.Wait()
-	logs.Debug("connection %v closed", conn.RemoteAddr())
+	logs.Debug("connection %v closed", targetConn.RemoteAddr())
 }
